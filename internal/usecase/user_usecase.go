965a82429025193851679e701be45149ac94eaea
diemensa <52234971+diemensa@users.noreply.github.com>
2025-05-25 11:00:14 +0300

    feat:  implement all usecases for UserService

--- internal/usecase/user_usecase.go
+++ internal/usecase/user_usecase.go
@@ -1,7 +1,10 @@
 package usecase
 
 import (
+	"context"
 	"github.com/diemensa/denezhki/internal/repository"
+	"github.com/diemensa/denezhki/internal/repository/postgres/model"
+	"github.com/google/uuid"
 )
 
 type UserService struct {
@@ -12,4 +15,34 @@ func NewUserService(u repository.UserRepo) *UserService {
 	return &UserService{
 		userRepo: u,
 	}
+}
+
+func (u *UserService) GetUser(c context.Context, username string) (*model.User, error) {
+
+	return u.userRepo.GetUser(c, username)
+
+}
+
+func (u *UserService) GetUserAccounts(c context.Context, userID uuid.UUID) ([]model.Account, error) {
+
+	return u.userRepo.GetUserAccounts(c, userID)
+
+}
+
+func (u *UserService) CreateUser(c context.Context, username, password string) error {
+
+	return u.userRepo.CreateUser(c, username, password)
+
+}
+
+func (u *UserService) CreateAccount(c context.Context, userID uuid.UUID) error {
+
+	return u.userRepo.CreateAccount(c, userID)
+
+}
+
+func (u *UserService) ValidatePassword(c context.Context, username, password string) error {
+
+	return u.userRepo.ValidatePassword(c, username, password)
+
 }
